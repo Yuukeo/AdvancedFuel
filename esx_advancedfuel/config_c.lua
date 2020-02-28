25d1c9b43f847369a59d761eaaf93b8eae5ecc37
CI-EVENTS <61483052+Grumpy-5murf@users.noreply.github.com>
2020-02-29 00:23:18 +0100

    Add files via upload

---
+++ esx_advancedfuel/config_c.lua
@@ -0,0 +1,39 @@
+lang = "fr"
+-- lang = "fr"
+
+settings = {}
+settings["en"] = {
+	openMenu = "Press ~g~E~w~ to open the menu.",
+	electricError = "~r~You have an electric vehicle.",
+	fuelError = "~r~You're not in the good place.",
+	buyFuel = "buy fuel",
+	liters = "liters",
+	percent = "percent",
+	confirm = "Confirm",
+	fuelStation = "Fuel station",
+	boatFuelStation = "Fuel station | Boat",
+	avionFuelStation = "Fuel station | Plane ",
+	heliFuelStation = "Fuel station | Helicopter",
+	price = "price"
+}
+
+settings["fr"] = {
+	openMenu = "Appuyez sur ~g~E~w~ pour ouvrir le menu.",
+	electricError = "~r~Vous avez une voiture électrique.",
+	fuelError = "~r~Vous n'êtes pas au bon endroit.",
+	buyFuel = "Acheter de l'essence",
+	liters = "Litres",
+	percent = "Pourcent",
+	confirm = "Valider",
+	fuelStation = "Station essence",
+	boatFuelStation = "Station d'essence | Bateau",
+	avionFuelStation = "Station d'essence | Avions",
+	heliFuelStation = "Station d'essence | Hélicoptères",
+	price = "Prix"
+}
+
+
+
+
+
+electricityPrice = 1 -- NOT RANDOMED !!!
